aaced280c9c0008bb79f2681dfe5bda045e8b143
Siwei LU <me@siwei.lu>
2023-10-24 23:02:54 +0800

    feat: make notify compact

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -48,14 +48,14 @@ local plugins = {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
-      -- add any options here
+      views = {
+        notify = {
+          render = "compact",
+        },
+      },
     },
     dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
     },
   },
